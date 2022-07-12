1700a4bc24b6241a2249f8f5eaa7a2a117725a2a
Jholauss Llerena <jholauss.07@hotmail.com>
2022-07-12 00:23:04 -0500

    Actualizando....

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import imagenes from '../assets/imagenes'
 
 const Error404 = () => {
+    const navegate= useNavigate();
   return (
-    <div>Error404</div>
+    <div style={styles.container} className="container" >
+        <img  src={imagenes.Error404} class="w-50 " alt="Error404"></img>
+        <div className='d-flex text-center'>
+        <button className='btn btn-danger' onClick={()=>navegate('/') }>Volver al inicio</button>
+        </div>
+    </div>
   )
 }
 
 export default Error404
+
+const styles = {
+    container:{
+        backgroundColor: '#f5f5f5',
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+
+}        
--- src/assets/imagenes.js
+++ src/assets/imagenes.js
@@ -1,9 +1,11 @@
 import slider1 from '../assets/slider1.jpg';
 import slider2 from '../assets/slider2.jpg';
 import slider3 from '../assets/slider3.jpg';
+import Error404 from '../assets/404.jpg';
 export default {
     "slider1": slider1,
     "slider2": slider2,
-    "slider3": slider3
+    "slider3": slider3,
+    "Error404": Error404
 }
 
